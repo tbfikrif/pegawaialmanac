cede3accee527658d2eee40cf18dd49af0541d95
anjas lesmana <32298465+alesna97@users.noreply.github.com>
2018-07-30 19:31:46 +0700

    Update conn_db.php

--- config/conn_db.php
+++ config/conn_db.php
@@ -1,17 +1,12 @@
 <?php
-	// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:pegawaialmanacdb.database.windows.net,1433; Database = pegawaialmanacdb", "almanac", "Tugasatol9");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "almanac@pegawaialmanacdb", "pwd" => "Tugasatol9", "Database" => "pegawaialmanacdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:pegawaialmanacdb.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
-
+	$host = 'pegawaialmanacdb';
+	$user = 'almanac';
+	$pass = 'Tugasatol9';
+	$db = 'dbkepegawaian';
+	// create connection
+	$conn = mysqli_connect($host,$user,$pass,$db);
+	// check connection
+	if(!$conn){
+		die("Connection Failed :".mysqli_connect_error());
+	}
 ?>
