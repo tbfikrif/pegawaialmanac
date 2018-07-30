a96cb93cda3720f520d7cd78b1eb50a16e823672
Tubagus Fikri Fatoni <tbfikrif@gmail.com>
2018-07-30 19:12:43 +0700

    Update conn_db.php

--- config/conn_db.php
+++ config/conn_db.php
@@ -1,12 +1,17 @@
 <?php
-	$host = 'pegawaialmanacdb.database.windows.net';
-	$user = 'almanac';
-	$pass = 'Tugasatol9';
-	$db = 'pegawaialmanacdb';
-	// create connection
-	$conn = mysqli_connect($host,$user,$pass,$db);
-	// check connection
-	if(!$conn){
-		die("Connection Failed :".mysqli_connect_error());
-	}
+	// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:pegawaialmanacdb.database.windows.net,1433; Database = pegawaialmanacdb", "almanac", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "almanac@pegawaialmanacdb", "pwd" => "{your_password_here}", "Database" => "pegawaialmanacdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:pegawaialmanacdb.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 ?>
