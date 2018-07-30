08d57d70ed54ff8fd596a44417ed0adadc4641aa
anjas lesmana <32298465+alesna97@users.noreply.github.com>
2018-07-30 20:23:38 +0700

    Update login_proc.php

--- config/login_proc.php
+++ config/login_proc.php
@@ -22,17 +22,4 @@
 				echo "$username,$pass";
 			}
 		}
-		/*
-			$host = 'localhost';
-			$user = 'root';
-			$pass = '';
-			$db = 'dbkepegawaian';
-			// create connection
-			$conn = mysqli_connect($host,$user,$pass,$db);
-			// check connection
-			if(!$conn){
-			die("Connection Failed :".mysqli_connect_error());
-			}
-		*/
-
 ?>
