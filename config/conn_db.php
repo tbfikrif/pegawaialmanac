f74645e49806a87788d7d5e5fdf61e36fc066b3e
anjas lesmana <32298465+alesna97@users.noreply.github.com>
2018-07-30 20:24:08 +0700

    Update conn_db.php

--- config/conn_db.php
+++ config/conn_db.php
@@ -13,4 +13,16 @@
 	$connectionInfo = array("UID" => "almanac@pegawaialmanacdb", "pwd" => "Tugasatol9", "Database" => "pegawaialmanacdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 	$serverName = "tcp:pegawaialmanacdb.database.windows.net,1433";
 	$conn = sqlsrv_connect($serverName, $connectionInfo);
+	/*
+			$host = 'localhost';
+			$user = 'root';
+			$pass = '';
+			$db = 'dbkepegawaian';
+			// create connection
+			$conn = mysqli_connect($host,$user,$pass,$db);
+			// check connection
+			if(!$conn){
+			die("Connection Failed :".mysqli_connect_error());
+			}
+		*/
 ?>
